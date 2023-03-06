3b7ed448bf56a474658abdab02cbd45fd01559f3
Ayub <egalayub2@gmail.com>
2023-03-06 14:08:07 -0500

    Created Pet Repository

---
+++ Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/PetController.java
@@ -0,0 +1,23 @@
+package com.wcci.virtualPetAPI.restControllers;
+
+import com.wcci.virtualPetAPI.entities.VirtualPet;
+import com.wcci.virtualPetAPI.repositories.PetRepository;
+import jdk.jfr.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PublicKey;
+
+@RestController
+public class PetController {
+    final private PetRepository petRepository;
+
+    public PetController (final @Autowired PetRepository petRepository){
+        this.petRepository = petRepository;
+    }
+    @GetMapping ("/pets")
+    public Iterable<VirtualPet> getPets (){
+        return petRepository.findAll();
+    }
+}
