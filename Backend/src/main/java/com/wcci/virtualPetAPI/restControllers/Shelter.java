c9ce3d85028701a2724c96380ded99d2c1304471
Adunia <atsehaie@outlook.com>
2023-03-06 13:47:16 -0500

    fixed errors

--- Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/Shelter.java
+++ Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/Shelter.java
@@ -1,7 +1,23 @@
 package com.wcci.virtualPetAPI.restControllers;
 
+import com.wcci.virtualPetAPI.entities.VirtualPetShelter;
+import com.wcci.virtualPetAPI.repositories.ShelterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Shelter {
+    private ShelterRepository shelterRepository;
+
+    public Shelter(final @Autowired ShelterRepository shelterRepository) {
+        this.shelterRepository = shelterRepository;
+    }
+
+    @GetMapping("/shelters")
+    public Iterable<VirtualPetShelter> getShelters() {
+        return shelterRepository.findAll();
+    }
 }
+
+
