7a328a312a7265293133c699a35f993893055c3b
Ayub <egalayub2@gmail.com>
2023-03-08 11:25:24 -0500

    Created endpoints in petController

--- Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/PetController.java
+++ Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/PetController.java
@@ -1,13 +1,19 @@
 package com.wcci.virtualPetAPI.restControllers;
 
+import com.wcci.virtualPetAPI.entities.RoboticDog;
 import com.wcci.virtualPetAPI.entities.VirtualPet;
 import com.wcci.virtualPetAPI.repositories.PetRepository;
 import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.boot.context.properties.source.InvalidConfigurationPropertyValueException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.PublicKey;
+import java.util.Optional;
 
 @RestController
 public class PetController {
@@ -17,7 +23,31 @@ public class PetController {
         this.petRepository = petRepository;
     }
     @GetMapping ("/pets")
-    public Iterable<VirtualPet> getPets (){
+    public Iterable<VirtualPet> getAllPets (){
         return petRepository.findAll();
     }
+    @GetMapping ("/pets/{petId}")
+    public VirtualPet getSinglePet (@PathVariable String petId){
+        return petRepository.findById(petId).get();
+    }
+    @DeleteMapping("pets/{petId}")
+    public void adoptPet (@PathVariable String petId){
+        Optional<VirtualPet> optionalVirtualPet = petRepository.findById(petId);
+optionalVirtualPet .ifPresentOrElse((VirtualPet) -> {
+    petRepository.delete(VirtualPet);
+        },
+        () -> {
+    throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Cannot Adopt non existing pet" + petId);
+        });
+    }
+    @PostMapping ("pets/{petId}")
+    public void admitPet (@PathVariable String petId, @RequestBody RoboticDog admitPet){
+      admitPet.getRoboticPetName();
+      admitPet.getHappiness();
+      admitPet.getHealth();
+      admitPet.oil();
+      admitPet.batteryLife();
+      petRepository.save(admitPet);
+    }
+
 }
--- Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/ShelterController.java
+++ Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/ShelterController.java
@@ -1,23 +1,32 @@
 package com.wcci.virtualPetAPI.restControllers;
 
+import com.wcci.virtualPetAPI.entities.VirtualOrganicPet;
 import com.wcci.virtualPetAPI.entities.VirtualPetShelter;
 import com.wcci.virtualPetAPI.repositories.ShelterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ShelterController {
-    private ShelterRepository shelterRepository;
+    private final ShelterRepository shelterRepository;
 
     public ShelterController(final @Autowired ShelterRepository shelterRepository) {
         this.shelterRepository = shelterRepository;
     }
 
     @GetMapping("/shelters")
-    public Iterable<VirtualPetShelter> getShelters() {
+    public Iterable<VirtualPetShelter> getAllShelters() {
         return shelterRepository.findAll();
     }
+
+   @GetMapping("/shelters/{shelterId}")
+    public VirtualPetShelter getPetShelter (@PathVariable String shelterId){
+    return shelterRepository.findById(shelterId).get();
+
+   }
+
 }
+
+
 
 
